9ac71f66652083b41ecc796d671ed11d1919f99c
Ashish Agarwal <ash.95cool@gmail.com>
2024-02-18 12:39:37 +0000

    Frontend main end to end

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,71 +1,47 @@
 import { useEffect, useState } from "react";
 import { TextInput, Badge } from "@canva/app-ui-kit";
+import ApiCalls from "./ApiCalls";
 
-export default function Home(){
-    const [loaded, setLoaded] = useState(false);
-    const [patientBackground, setPatientBackground] = useState('Meet Alex, a 28-year-old software engineer with a promising career but a hidden struggle with mental health. Growing up in a high-achieving family, Alex faced immense pressure to excel academically. This pressure intensified during college, where expectations for success were a constant source of stress.Throughout early adulthood, Alexs mental health deteriorated, marked by chronic anxiety and self-doubt. The demanding work environment in the tech industry exacerbated these issues, leading to burnout and a sense of isolation. Despite outward success, internally, Alex felt like an imposter, haunted by a fear of failure.The breaking point came when a major project deadline coincided with a personal loss. Alex began experiencing severe panic attacks and difficulty concentrating, impacting both personal and professional life. Friends and colleagues were unaware of this silent struggle.Seeking help, Alex started therapy, revealing the deep-rooted perfectionism and the toll it took on mental well-being. With ongoing support, Alex is gradually learning healthier coping mechanisms and striving to redefine success beyond external expectations. The journey is ongoing, illustrating the complex interplay between high-achieving environments and mental health challenges.');
-    const [suggestedReply, setSuggestedReply] = useState("I'm truly sorry to hear that you're experiencing depression. It's a courageous step to acknowledge and share what you're going through. Depression can feel overwhelming and isolating, but please know that you're not alone in this. There are many paths to healing and support available to you. First and foremost, it's important to prioritize your well-being. Seeking professional help from a therapist or counselor who specializes in depression can provide valuable insights and strategies for managing symptoms. They can offer a safe space for you to explore your emotions and develop coping mechanisms tailored to your needs. Additionally, medication prescribed by a psychiatrist may be part of your treatment plan if deemed appropriate. It's essential to have open and honest communication with your healthcare provider to find the right approach for you.");
+export default function Home() {
+    const [dummyUserReplies, setDummyUserReplies] = useState([
+        "Hello. I am feeling depressed these days",
+        "I'm worried about starting therapy. What if it doesn't help? What if I'm just beyond help?",
+        "Thank you for your understanding and support. It means a lot to me to know that I'm not alone in this struggle.",
+        "I want to believe that things can get better, but right now, it's hard to see a light at the end of the tunnel.",
+        "There are moments when I feel guilty for feeling this way. I have so much to be grateful for, yet I still can't shake off this sadness",
+        "Thank you for reminding me that there's hope. I'll try to hold onto that, even on the darkest days."
+    ]);
+    const [patientBackground, setPatientBackground] = useState('Summary will appear here after Document Upload.');
+    const [suggestedReply, setSuggestedReply] = useState("");
     const [uploadSuccess, setUploadSuccess] = useState(false);
-  
-    const [file, setFile] = useState<File | null>(null);
-  
-  
-    useEffect(() => {
-      const timeout = setTimeout(() => {
-        setLoaded(true);
-      }, 100);
-      return () => clearTimeout(timeout);
-    }, []);
+    const [file, setFile] = useState(null);
+    const [userId, setUserId] = useState("user-111");
+    const [chatHistory, setChatHistory] = useState([]);
+    const [doctorChatTypeText, setDoctorChatTypeText] = useState("");
   
     useEffect(() => {
-      if (file === null) {
-        const timeoutId = setTimeout(() => {
-          // Perform some action after waiting
-          console.log('Waiting for file...');
-        }, 1000);
-  
-        return () => clearTimeout(timeoutId);
+      if (file !== null) {
+        handleFileSubmit();
       }
     }, [file]);
   
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => { // Explicitly type the event parameter
-      if (e.target.files && e.target.files.length > 0) {
-        setFile(e.target.files[0]);
-        
-        handleSubmit(); // Auto-submit the form when a file is selected
-      }
-    };
-  
-    const handleSubmit = async () => {
-  
-      if (!file) return; // Prevent submission if no file is selected
-      setUploadSuccess(true);    
-      const formData = new FormData();
-      formData.append('pdfFile', file);
-  
-      try {
-        const response = await fetch('http://localhost:5000/upload-pdf', {
-          method: 'POST',
-          body: formData
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          console.log("Upload was successful");
-        } else {
-          console.error('Error uploading PDF file');
+    const handleFileChange = (e: any) => {
+        e.preventDefault();
+        if (e.target.files && e.target.files.length > 0) {
+            setFile(e.target.files[0]);
         }
-      } catch (error) {
-        console.error('Network error:', error);
-      }
     };
-    
-    const myStyle: React.CSSProperties = {
-      opacity: loaded ? 1 : 0,
+  
+    const handleFileSubmit = async () => {
+        if (!file) return;
+        setUploadSuccess(true);
+        const formData = new FormData();
+        formData.append('pdfFile', file);
+        let summary = await ApiCalls.startSession(userId, formData);
+        setPatientBackground(summary);
     };
 
-    const getPatientReply = (name: any, reply: any) => {
+    const getPatientReplyBox = (name: any, reply: any) => {
         return (
             <div className="d-flex align-items-center">
                 <div className="text-left pr-1"><img src="https://img.icons8.com/color/40/000000/guest-female.png" width="30" className="img1" /></div>
@@ -77,7 +53,7 @@ export default function Home(){
         )
     };
 
-    const getDoctorReply = (name: any, reply: any) => {
+    const getDoctorReplyBox = (name: any, reply: any) => {
         return (
             <div className="d-flex align-items-center text-right justify-content-end ">
                 <div className="pr-2">
@@ -89,19 +65,61 @@ export default function Home(){
         )
     };
 
-    const handleChatKeyDown = (e: any) => {
+    const constructChatBox = () => {
+        if(chatHistory.length === 0) {
+            return <div style={{marginTop: '30%'}} className="empty-chat">
+                Your chat will appear here
+            </div>
+        }
+
+        return <>
+            {chatHistory.map((chat: any) => {
+                if(chat.party === "patient") {
+                    return getPatientReplyBox("Alex Michael", chat.reply);
+                }
+                else {
+                    return getDoctorReplyBox("Dr. Hendrikson", chat.reply);
+                }
+            })}
+        </>
+    };
+
+    const handleDoctorChatTypeText = (textContent: any) => {
+        setDoctorChatTypeText(textContent);
+    };
+
+    const handleChatKeyDown = async (e: any) => {
         if(e.key === "Enter") {
-            console.log("Entered");
+            let chatHistoryNew: any = [...chatHistory];
+            chatHistoryNew.push({party: "doctor", reply: doctorChatTypeText});
+            setChatHistory(chatHistoryNew);
+            let lastUserReply = chatHistoryNew.findLast((chat: any) => chat.party === "patient");
+            ApiCalls.saveConversation(userId, (lastUserReply ? lastUserReply.reply : ""), doctorChatTypeText);
+
+            if(dummyUserReplies.length > 0) {
+                let newUserReply = dummyUserReplies[0];
+                chatHistoryNew.push({ party: "patient", reply: newUserReply });
+                setChatHistory(chatHistoryNew.slice(0));
+                setDummyUserReplies(dummyUserReplies.slice(1));
+                setSuggestedReply("...Loading...");
+                let suggestedReply = await ApiCalls.ongoingSession(userId, newUserReply);
+                setSuggestedReply(suggestedReply);
+            }
         }
     };
+
+    const handleSuggestedReplyClick = (e: any) => {
+        e.preventDefault();
+        setDoctorChatTypeText(suggestedReply);
+    }
   
     return (   
-    <div style={myStyle} className="home-style">
+    <div className="home-style">
       <div className="home-header">
         <h1 className="header-text">Welcome to <b>WeHeal</b>. The companion for superheroes.</h1>
         <div className="header-upload">
             <div>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleFileChange}>
                     {uploadSuccess ? (
                         <img src={'./checked.png'} alt="Success" style={{ width: '70px', height: '70px' }} />
                     ) : (
@@ -110,7 +128,6 @@ export default function Home(){
                         </label>
                     )}
                     <input id="file-upload" type="file" onChange={handleFileChange} style={{ display: 'none' }} />
-                    {!uploadSuccess && <button type="submit" style={{ display: 'none' }}></button>}
                 </form>
             </div>
             <p>Upload Pre-Session Questionaire</p>
@@ -121,35 +138,37 @@ export default function Home(){
             <h1 style={{ paddingTop: '20px' }}>Patient Background</h1>
             <p style={{width:'90%'}}>{patientBackground}</p>
             <h1 style={{ paddingTop: '20px' }}>Suggested Reply</h1>
-            <span className="suggested-reply">{suggestedReply}</span>
+            <span className="suggested-reply" onClick={handleSuggestedReplyClick}>{suggestedReply}</span>
         </div>
 
         <div className="home-content-right">
             <div className="home-content-right-content" style={{ width: '90%', height: '80%', paddingLeft: '20px' }}>
                 <div className="d-flex justify-content-start" >
                     <div className="px-2 scroll with-padding" style={{ width: '100%', height: '100%' }}>
-                        {getPatientReply("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
-                        {getDoctorReply("Dr. Hendrikson", "Let's jump on a video call")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
-                        {getDoctorReply("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
-                        {getPatientReply("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
-                        {getDoctorReply("Dr. Hendrikson", "Let's jump on a video call")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
-                        {getDoctorReply("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
-                        {getPatientReply("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
-                        {getDoctorReply("Dr. Hendrikson", "Let's jump on a video call")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
-                        {getDoctorReply("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
-                        {getPatientReply("Sarah Anderson", "How often should I take this?")}
+                        {/* {getPatientReplyBox("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Let's jump on a video call")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")}
+                        {getPatientReplyBox("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Let's jump on a video call")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")}
+                        {getPatientReplyBox("Sarah Anderson", "Hi Dr. Hendrikson, I haven't been feeling well for the past few days.")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Let's jump on a video call")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")}
+                        {getDoctorReplyBox("Dr. Hendrikson", "Twice a day, at breakfast and before bed")}
+                        {getPatientReplyBox("Sarah Anderson", "How often should I take this?")} */}
+                        {constructChatBox()}
                     </div>
                 </div>
                 <div className="chat-input-doctor">
                     <TextInput
-                        onChange={function noRefCheck(){}}
+                        onChange={handleDoctorChatTypeText}
                         onKeyDown={handleChatKeyDown}
                         placeholder="Type your reply and press Enter"
+                        value={doctorChatTypeText}
                     />
                 </div>
             </div>
